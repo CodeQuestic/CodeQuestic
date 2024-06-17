70bc7803d47a629458166391aad00d4464c30db3
Rakshith Acharya <rakshixh@gmail.com>
2024-06-17 15:04:36 +0530

    updated landing page

---
+++ src/v1/src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+function LandingPage() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default LandingPage;
