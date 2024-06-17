ec1c92b8dd36c5ecb32b9a436e6e2d5b726171db
Rakshith Acharya <rakshixh@gmail.com>
2024-06-17 15:04:12 +0530

    updated header component

---
+++ src/v1/src/components/Header.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import HeaderCSS from "../css/Header.module.css";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <header className={HeaderCSS.header}>
+        <div>
+          <Link to="/" className={HeaderCSS.logotext}>
+            CodeQuestic
+          </Link>
+        </div>
+        <div className={HeaderCSS.menuIcon} onClick={toggleMenu}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </div>
+        <ul
+          className={`${HeaderCSS.ulist} ${menuOpen ? HeaderCSS.menuOpen : ""}`}
+        >
+          <li className={HeaderCSS.list}>
+            <Link to="/" className={HeaderCSS.atag}>
+              Home
+            </Link>
+          </li>
+          <li className={HeaderCSS.list}>
+            <a
+              href="https://github.com/CodeQuestic"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={HeaderCSS.atag}
+            >
+              GitHub
+            </a>
+          </li>
+          <li className={HeaderCSS.list}>
+            <a
+              href="https://discord.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={HeaderCSS.atag}
+            >
+              Discord
+            </a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+}
+
+export default Header;
