b90e66d6a6d934985074c9c9819ce4172efe234b
Rakshith Acharya <rakshixh@gmail.com>
2024-06-17 15:36:13 +0530

    added herosection to lamdingpage

--- src/v1/src/pages/LandingPage.js
+++ src/v1/src/pages/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
 
 function LandingPage() {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   );
 }
